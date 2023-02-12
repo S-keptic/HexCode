472f81383e93bb9d7ec70ef7e4b5b2c668268de1
sekire <69573771+leviackermannn@users.noreply.github.com>
2023-02-12 23:54:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let btn = document.getElementById("myBtn");
+
+btn.onclick = ()=> {
+{
+  let hex_numbers = ["0","1","2","3","4","5","6","7","8","9","A", "B", "C", "D", "E", "F"];
+
+  let hexcode = '';
+  
+  for(let i = 0; i<6; i++){
+    let random_index = Math.floor(Math.random() * hex_numbers.length);
+    hexcode += hex_numbers[random_index];
+  }
+
+  document.getElementById("hex-code").innerHTML = hexcode;
+  document.body.style.backgroundColor = '#' + hexcode;
+}}
